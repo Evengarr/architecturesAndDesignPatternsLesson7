80e7a9643790e927d41941cf9a4e87e302fa5fe4
Evengarr <Ravasect@gmail.ru>
2021-10-10 19:52:01 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/patternslesson7/ArchitecturesAndDesignPatternsLesson7Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.patternslesson7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturesAndDesignPatternsLesson7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitecturesAndDesignPatternsLesson7Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/patternslesson7/ArchitecturesAndDesignPatternsLesson7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.patternslesson7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturesAndDesignPatternsLesson7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
